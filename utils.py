333b5d07045e7685ae962e55d308035e88dee730
funyard <ziga.zp2005@gmail.com>
2022-02-01 17:17:37 +0100

    Added documentation

--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ from typing import List
 
 
 def fill_circle(x, y, r) -> List[List]:
+    """
+    Function which loops through every pixel in pygame window. If the pixel is inside the circle, it adds the pixel's coordinates to the list of pixels, which is returned.
+    """
+
     width = x
     height = y
     list_of_coords = []
@@ -15,6 +19,10 @@ def fill_circle(x, y, r) -> List[List]:
 
 
 def midpoint_circle_draw(x, y, r) -> List[List]:
+    """
+    Function loops through every pixel in pygame window. If the pixel's coordinates squared and added together equal 1, it is added to the list of pixels, which is returned.
+    NOTE: Because the coordinates are usualy not exactly 0, there is also a list, which contains the deviation.
+    """
     width = x
     height = y
     list_of_coords = []
@@ -26,5 +34,3 @@ def midpoint_circle_draw(x, y, r) -> List[List]:
                 list_of_coords.append([x, y])
 
     return list_of_coords
-
-
