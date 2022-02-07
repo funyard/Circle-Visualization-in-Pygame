717e1c1ba33ece6e5ca6fa5e89b1bd9fb2e4746b
zigaz <ziga.zp2005@gmail.com>
2022-02-07 12:46:44 +0100

    Created widgets

--- main.py
+++ main.py
@@ -6,38 +6,37 @@ import utils
 import string
 import classes
 
-# from pygame_widgets.button import Button
-# import pygame_widgets
+from pygame_widgets.button import Button
+import pygame_widgets
 
 
 #! Not implemented yet!
-"""
-def start_button_func():
+def start_button_func(radius, surface, bg, start_button):
     filled = False
+    start_button.hide()
     try:
         if filled == None:
             filled = True
 
         try:
-            radius = int(radius_input_field_rect.string_of_keys)
+            radius = int(radius.string_of_keys)
         except ValueError:
             radius = 200
 
-        screen.blit(background, (0, 0))
+        surface.blit(bg, (0, 0))
         print(radius)
         if filled:
             list_of_filled_rect_coords = utils.fill_circle(WIDTH, HEIGHT, radius)
             for el in list_of_filled_rect_coords:
-                pygame.draw.rect(screen, BLACK, pygame.Rect(el[0], el[1], 1, 1))
+                pygame.draw.rect(surface, (0, 0, 0), pygame.Rect(el[0], el[1], 1, 1))
         if not filled:
             list_of_rect_coords = utils.better_midpoint_circle_draw(
                 WIDTH, HEIGHT, radius
             )
             for el in list_of_rect_coords:
-                pygame.draw.rect(screen, BLACK, pygame.Rect(el[0], el[1], 1, 1))
+                pygame.draw.rect(surface, (0, 0, 0), pygame.Rect(el[0], el[1], 1, 1))
     except Exception as e:
         print("Exception: ", e)
-"""
 
 
 def main() -> None:
@@ -45,6 +44,7 @@ def main() -> None:
     pygame.init()
 
     # VARS
+    global WIDTH, HEIGHT
     WIDTH, HEIGHT = 800, 600
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
@@ -62,44 +62,69 @@ def main() -> None:
     background.fill(WHITE)
 
     # Create main menu
-    start_button = pygame.Rect(400 - 200 / 2, 440, 200, 100)
     #! Not implemented yet!
-    """start_button = Button(
+    start_button = Button(
         screen,
         400 - 200 / 2,
         440,
         200,
         100,
         text="START",
+        textColour=WHITE,
         fontsize=34,
         margin=20,
-        inactiveColour=(255, 0, 0),
-        pressedColour=(0, 255, 0),
-        radius=20,
-        onClick=start_button_func,
+        inactiveColour=BLUE,
+        pressedColour=(20, 20, 90),
+        onClick=lambda: start_button_func(
+            radius_input_field_rect, screen, background, start_button
+        ),
+        radius=4
     )
-    """
-    filled_circle_button = pygame.Rect(150, 300, 200, 100)
-    hollow_circle_button = pygame.Rect(450, 300, 200, 100)
+    filled_circle_button = Button(
+        screen,
+        150,
+        300,
+        200,
+        100,
+        text="FILLED CIRCLE",
+        textColour=WHITE,
+        fontsize=34,
+        margin=20,
+        inactiveColour=GRAY,
+        pressedColour=(20, 20, 90),
+        onClick=,
+        radius=4,
+        
+    )
+    hollow_circle_button = Button(
+        screen,
+        450,
+        300,
+        200,
+        100,
+        text="HOLLOW CIRCLE",
+        textColour=WHITE,
+        fontsize=34,
+        margin=20,
+        inactiveColour=GRAY,
+        pressedColour=(20, 20, 90),
+        radius=4,
+        onClick=lambda: filled = False
+    )
+    # hollow_circle_button = pygame.Rect(450, 300, 200, 100)
     radius_input_field_rect = classes.InputBox(screen, 150, 200, 500, 60, BLACK)
 
     # Draw all fonts
     title_text = TITLE_FONT.render("Midpoint circle visualization", True, BLACK)
-    start_button_text = BUTTON_FONT.render("START", True, WHITE)
-    filled_button_text = BUTTON_FONT.render("FILLED CIRCLE", True, WHITE)
-    hollow_button_text = BUTTON_FONT.render("HOLLOW CIRCLE", True, WHITE)
+    # hollow_button_text = BUTTON_FONT.render("HOLLOW CIRCLE", True, WHITE)
 
     # Blit everything to the screen
     screen.blit(background, (0, 0))
 
-    pygame.draw.rect(screen, BLUE, start_button)
-    pygame.draw.rect(screen, GRAY, filled_circle_button)
-    pygame.draw.rect(screen, GRAY, hollow_circle_button)
+    # pygame.draw.rect(screen, GRAY, hollow_circle_button)
 
     screen.blit(title_text, (120, 100))
-    screen.blit(start_button_text, ((400 - 200 / 2) + 65, 440 + 40))
-    screen.blit(hollow_button_text, (450 + 2, 300 + 40))
-    screen.blit(filled_button_text, (150 + 13, 300 + 40))
+    # screen.blit(hollow_button_text, (450 + 2, 300 + 40))
     radius_input_field_rect.draw()
 
     # Event loop
@@ -111,39 +136,6 @@ def main() -> None:
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_coords = pygame.mouse.get_pos()
-
-                # Over start button
-
-                if utils.is_over(start_button, (mouse_coords[0], mouse_coords[1])):
-                    try:
-                        if filled == None:
-                            filled = True
-
-                        try:
-                            radius = int(radius_input_field_rect.string_of_keys)
-                        except ValueError:
-                            radius = 200
-
-                        screen.blit(background, (0, 0))
-                        print(radius)
-                        if filled:
-                            list_of_filled_rect_coords = utils.fill_circle(
-                                WIDTH, HEIGHT, radius
-                            )
-                            for el in list_of_filled_rect_coords:
-                                pygame.draw.rect(
-                                    screen, BLACK, pygame.Rect(el[0], el[1], 1, 1)
-                                )
-                        if not filled:
-                            list_of_rect_coords = utils.better_midpoint_circle_draw(
-                                WIDTH, HEIGHT, radius
-                            )
-                            for el in list_of_rect_coords:
-                                pygame.draw.rect(
-                                    screen, BLACK, pygame.Rect(el[0], el[1], 1, 1)
-                                )
-                    except Exception as e:
-                        print("Exception: ", e)
 
                 # Over filled circle button
                 if utils.is_over(
@@ -166,7 +158,7 @@ def main() -> None:
                     radius_input_field_rect.update(event.unicode)
 
         pygame.display.update()
-        # pygame_widgets.update(event)
+        pygame_widgets.update(event)
 
 
 if __name__ == "__main__":
