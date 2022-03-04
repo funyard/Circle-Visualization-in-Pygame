f40888b4deb14548c4b7231dccffc1e8e12e8529
zigaz <ziga.zp2005@gmail.com>
2022-03-04 10:26:28 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 #!python3
 
-import pygame
-import sys
-import utils
 import string
-import classes
+import sys
 
-from pygame_widgets.button import Button
+import pygame
 import pygame_widgets
+from pygame_widgets.button import Button
+
+import classes
+import utils
 
 
 def main() -> None:
-
     pygame.init()
 
     def set_circle_type(value):
@@ -22,11 +22,11 @@ def main() -> None:
     global WIDTH, HEIGHT, filled
     filled = None
     WIDTH, HEIGHT = 800, 600
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    BLUE = (0, 0, 240)
-    GRAY = (15, 15, 15)
-    TITLE_FONT = pygame.font.Font(None, 60)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    blue = (0, 0, 240)
+    gray = (15, 15, 15)
+    title_font = pygame.font.Font(None, 60)
 
     # Initialise screen
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -34,20 +34,20 @@ def main() -> None:
 
     # Fill background
     background = pygame.Surface(screen.get_size())
-    background.fill(WHITE)
+    background.fill(white)
 
     # Create main menu
     start_button = Button(
         screen,
-        400 - 200 / 2,
+        int(400 - 200 / 2),
         440,
         200,
         100,
         text="START",
-        textColour=WHITE,
+        textColour=white,
         fontsize=34,
         margin=20,
-        inactiveColour=BLUE,
+        inactiveColour=blue,
         pressedColour=(20, 20, 90),
         onClick=lambda: utils.start_button_func(
             radius_input_field_rect,
@@ -67,10 +67,10 @@ def main() -> None:
         200,
         100,
         text="FILLED CIRCLE",
-        textColour=WHITE,
+        textColour=white,
         fontsize=34,
         margin=20,
-        inactiveColour=GRAY,
+        inactiveColour=gray,
         pressedColour=(20, 20, 90),
         onClick=lambda: set_circle_type(True),
         radius=4,
@@ -82,18 +82,18 @@ def main() -> None:
         200,
         100,
         text="HOLLOW CIRCLE",
-        textColour=WHITE,
+        textColour=white,
         fontsize=34,
         margin=20,
-        inactiveColour=GRAY,
+        inactiveColour=gray,
         pressedColour=(20, 20, 90),
         radius=4,
         onClick=lambda: set_circle_type(False),
     )
-    radius_input_field_rect = classes.InputBox(screen, 150, 200, 500, 60, BLACK)
+    radius_input_field_rect = classes.InputBox(screen, 150, 200, 500, 60, black)
 
     # Draw all fonts
-    title_text = TITLE_FONT.render("Midpoint circle visualization", True, BLACK)
+    title_text = title_font.render("Midpoint circle visualization", True, black)
 
     # Blit everything to the screen
     screen.blit(background, (0, 0))
@@ -115,8 +115,8 @@ def main() -> None:
                 if event.unicode in string.digits:
                     radius_input_field_rect.update(event.unicode)
 
-        pygame.display.update()
-        pygame_widgets.update(event)
+            pygame_widgets.update(event)
+            pygame.display.update()
 
 
 if __name__ == "__main__":
